d038a7c059da408e231bcd92a1d234960f7c8400
yoeldeorte-beep <yoel.deorte@puentemaipo.cl>
2026-09-01 10:48:11 -0400

    Base proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+const app = express()
+
+
+//RUTAS
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/servicios'), (req,res) => {
+    res.send('servicios');
+}
+
+// levanta servidor
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
+
+// arrancar servidor
+//OP 1: node app.js
+//OP 2: npm dev
+//OP 3:
